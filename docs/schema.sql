b469cce6b5a66ff892dbe8539f365aa94c2db3b6
windsor.liu <windsor.liu@shoalter>
2023-09-11 10:28:08 +0800

    update db schema

---
+++ docs/schema.sql
@@ -0,0 +1,41 @@
+create database mall;
+
+-- user
+
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `address` varchar(256) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_modified_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+);
+
+
+-- order
+
+CREATE TABLE `order` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `total_amount` int NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_modified_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`)
+);
+
+-- order_item
+
+CREATE TABLE `order_item` (
+  `order_item_id` int NOT NULL AUTO_INCREMENT,
+  `order_item_key` varchar(200) NOT NULL,
+  `order_id` int NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `ice` varchar(20) NOT NULL,
+  `sweetness` varchar(20) NOT NULL,
+  `quantity` int NOT NULL,
+  `price` int NOT NULL,
+  `item_total_price` int NOT NULL,
+  PRIMARY KEY (`order_item_id`),
+  UNIQUE KEY `order_item_key` (`order_item_key`)
+);
